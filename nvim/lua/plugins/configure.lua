001b69983866c477fc9664c7da922fb5c283e38f
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-01-29 19:08:14 +0530

    blink.cmp

--- nvim/lua/plugins/configure.lua
+++ nvim/lua/plugins/configure.lua
@@ -23,6 +23,7 @@ vim.opt.fillchars = {
 -- vim.cmd [[hi foldcolumn guibg=bg]]
 -- vim.cmd [[hi foldcolumn guifg=#3b3b3b]]
 --
+
 vim.opt.background = "dark"
 vim.cmd('colorscheme kanagawa')
 -- -- lualine setup
@@ -102,7 +103,7 @@ require("mason-lspconfig").setup {
     ensure_installed = { 'clangd', 'pyright', 'ts_ls', 'lua_ls', 'taplo' }
 }
 -- -- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- local capabilities = require("blink.cmp").get_lsp_capabilities()
 local lspconfig = require('lspconfig')
 -- -- custom onattach
 local on_attach = function(client, bufnr) end
@@ -111,48 +112,8 @@ local servers = { 'clangd', 'pyright', 'ts_ls', 'lua_ls', 'taplo' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
-        capabilities = capabilities,
         root_dir = function(fname)
             return vim.loop.cwd()
         end
     }
 end
--- -- luasnip setup
-local luasnip = require 'luasnip'
--- -- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-    snippet = { expand = function(args) luasnip.lsp_expand(args.body) end, },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
-        -- C-b (back) C-f (forward) for snippet placeholder navigation.
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
-}
--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -12,9 +12,55 @@ return {
     {
         'goolord/alpha-nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-            require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
-        end
+        opts = function()
+            local dashboard = require("alpha.themes.dashboard")
+            local logo = [[
+                                  .
+ .:......                       .:-.
+::-==-:--::..                 ..:--.
+ ...::-------:..             .:---:.
+      ..-------:..          :=----:.
+         .=+++==-:..     .-++==--:.
+           -***++=-:.  .-=+++=--...
+            :-=+*+=-:.:==++==-:...:
+           ::::::++==++++++++++=------:::..
+                ...:-=--........:-+***+++===:.
+                                   .=++======...
+                                    :=----=-..--
+                                  ..:::::::..:.
+                                .. ...
+                                  ..
+]]
+            dashboard.section.header.val = vim.split(logo, "\n")
+            dashboard.section.buttons.val = {
+                dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+                dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+                dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+                dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+                dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+                dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+            }
+            dashboard.section.header.opts.hl = "AlphaHeader"
+            dashboard.opts.layout[1].val = 2
+            return dashboard
+        end,
+        config = function(_, dashboard)
+            require("alpha").setup(dashboard.opts)
+            vim.api.nvim_create_autocmd("User", {
+                callback = function()
+                    local stats = require("lazy").stats()
+                    local ms = math.floor(stats.startuptime * 100) / 100
+                    dashboard.section.footer.val = "󱐌 Lazy-loaded "
+                        .. stats.loaded
+                        .. "/"
+                        .. stats.count
+                        .. " plugins in "
+                        .. ms
+                        .. "ms"
+                    pcall(vim.cmd.AlphaRedraw)
+                end,
+            })
+        end,
     },
     'https://github.com/ryanoasis/vim-devicons',
     -- -- themes
@@ -143,13 +189,60 @@ return {
     'https://github.com/neovim/nvim-lspconfig',
     -- -- mason configuraton with lsp
     'williamboman/mason-lspconfig.nvim',
-    -- -- automcomplete
-    'hrsh7th/nvim-cmp',
     -- -- lsp autocomplete
-    'hrsh7th/cmp-nvim-lsp',
-    -- -- snippets
-    'saadparwaiz1/cmp_luasnip',
-    'L3MON4D3/LuaSnip',
+    {
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts = {
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono',
+                kind_icons = {
+                    Text = "",
+                    Method = "󰡱",
+                    Function = "󰊕",
+                    Constructor = "",
+                    Field = "",
+                    Variable = "󰂡",
+                    Class = "",
+                    Interface = "",
+                    Module = "",
+                    Property = "󰜢",
+                    Unit = "",
+                    Value = "󰎠",
+                    Enum = "",
+                    Keyword = "󰌋",
+                    Snippet = "󰒕",
+                    Color = "󰏘",
+                    Reference = "",
+                    File = "",
+                    Folder = "󰉋",
+                    EnumMember = "",
+                    Constant = "󰏿",
+                    Struct = "",
+                    Event = "",
+                    Operator = "󰆕",
+                    TypeParameter = "󰅲",
+                },
+            },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                cmdline = {}
+            },
+            completion = {
+                accept = {
+                    auto_brackets = { enabled = false },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                    treesitter_highlighting = false,
+                },
+            },
+        },
+        opts_extend = { "sources.default" }
+    },
     -- -- better rust support
     {
         'mrcjkb/rustaceanvim',
