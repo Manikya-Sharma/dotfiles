5e4b5bbac3a3c589a171ef9d6fbeb59d94a0bb1c
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-08-24 13:54:25 +0530

    glimmer

--- nvim/lua/configure/utils/autocmds.lua
+++ nvim/lua/configure/utils/autocmds.lua
@@ -1,9 +1,11 @@
 -- yank highlight
-vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", {}),
-	desc = "Hightlight selection on yank",
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
-	end,
-})
+if not Fancy then
+	vim.api.nvim_create_autocmd("TextYankPost", {
+		group = vim.api.nvim_create_augroup("highlight_yank", {}),
+		desc = "Hightlight selection on yank",
+		pattern = "*",
+		callback = function()
+			vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
+		end,
+	})
+end
