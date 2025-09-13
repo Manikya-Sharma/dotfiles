414caf8254641d6f1bddf15d367bbb0d5ea93b60
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2025-09-13 12:55:27 +0530

    faster telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,10 +2,22 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
-	opts = {
-		defaults = {
-			file_ignore_patterns = { "node_modules" },
-		},
-	},
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules" },
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+		require("telescope").load_extension("fzf")
+	end,
 }
