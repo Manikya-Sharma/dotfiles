49ea584a6402d48e00920f6138d60b8152c7a492
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2026-01-02 11:34:33 +0530

    updates + small fixes

--- nvim/lua/plugins/looks/treesitter.lua
+++ nvim/lua/plugins/looks/treesitter.lua
@@ -5,21 +5,21 @@ return {
 		build = ":TSUpdate",
 	},
 	-- use until vim.lsp.buf.selection_range is stabilized
-	{
-		{
-			"MeanderingProgrammer/treesitter-modules.nvim",
-			dependencies = { "nvim-treesitter/nvim-treesitter" },
-			opts = {
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<A-o>",
-						node_incremental = "<A-o>",
-						scope_incremental = "<A-O>",
-						node_decremental = "<A-i>",
-					},
-				},
-			},
-		},
-	},
+	-- {
+	-- 	{
+	-- 		"MeanderingProgrammer/treesitter-modules.nvim",
+	-- 		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	-- 		opts = {
+	-- 			incremental_selection = {
+	-- 				enable = true,
+	-- 				keymaps = {
+	-- 					init_selection = "<A-o>",
+	-- 					node_incremental = "<A-o>",
+	-- 					scope_incremental = "<A-O>",
+	-- 					node_decremental = "<A-i>",
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
--- nvim/lua/plugins/utils/fzf-lua.lua
+++ nvim/lua/plugins/utils/fzf-lua.lua
@@ -6,6 +6,9 @@ return {
 			fullscreen = true,
 		},
 		keymap = {
+			fzf = {
+				["ctrl-q"] = "select-all+accept",
+			},
 			builtin = {
 				["<C-E>"] = "preview-down",
 				["<C-Y>"] = "preview-up",
